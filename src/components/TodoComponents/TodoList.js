0cb305f9e7b57d438e87a5369657926d74e523c3
David White <dwhite0627@gmail.com>
2020-02-11 21:58:43 -0500

    Add TodoList component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+  
+  return (
+    <div className='todo-list'>
+      <div>
+        {props.todos.map(todo => (
+          <Todo key={todo.id} data={todo} toggleCompleted={props.toggleCompleted}/>
+        ))}
+        <button className='btn-clear' onClick={props.clearCompleted}>
+          Clear Completed
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default TodoList;
