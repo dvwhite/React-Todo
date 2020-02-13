c577039b2db50826fbc447730bc46b50f844be3d
David White <dwhite0627@gmail.com>
2020-02-12 22:57:02 -0500

    Center main app, change todo styles, add footer section, add green checkmark svg

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Todo = props => {
-  console.log(props);
   return (
     <div className={`todo${props.data.completed ? " completed" : ""}`} onClick={() => props.toggleCompleted(props.data.id)}>
       <p>{props.data.task}</p>
+      {props.data.completed ? <img src='./../../images/checkmark.svg' alt='green checkmark' className='checkmark'/> : null}
     </div>
   )
 }
